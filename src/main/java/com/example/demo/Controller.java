dba65211d0b20fe20b3a2e8f0037bf003f43e306
lucasramostech_github <lucasramosstar66@gmail.com>
2026-09-09 18:02:01 -0300

    Updating Controller

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,6 +3,8 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
@@ -10,11 +12,14 @@ import org.springframework.http.HttpStatus;
 @RestController
 public class Controller {
 
-    @GetMapping({"/", "/api/hello"})
+    @GetMapping("/")
     public ResponseEntity<String> hello() {
         return new ResponseEntity<>("Portfolio Builder!", HttpStatus.OK);
     }
 
-    
+    @PostMapping("/api/hello")
+    public ResponseEntity<DatasRequest> receberDados(@RequestBody DatasRequest request) {
 
+        return new ResponseEntity<>(request, HttpStatus.OK);
+    }
 }
