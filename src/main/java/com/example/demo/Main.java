4ebf2622e5e5dabe49617da16f0bcd81ed12d59f
lucasramostech_github <lucasramosstar66@gmail.com>
2026-09-10 16:51:31 -0300

    Fixing the Database

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,15 +1,21 @@
 package com.example.demo; 
 
-// imports
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication; 
 
-// tag 
-@SpringBootApplication 
 
-// classe
+// Tags
+@SpringBootApplication 
 public class Main {
     public static void main(String[] args) { 
+        
+        // COnfig do env
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> 
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(Main.class, args); 
-    }    
+    }     
 }
