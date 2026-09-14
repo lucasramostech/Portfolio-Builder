84f50339b6a73fa99c10941c4a0270520ba60ed4
lucasramostech_github <lucasramosstar66@gmail.com>
2026-09-14 15:27:30 -0300

    Updating MainService

--- src/main/java/com/example/demo/MainService.java
+++ src/main/java/com/example/demo/MainService.java
@@ -16,15 +16,34 @@ public class MainService {
     }
 
     // Método principal do software
-    public List<Feedback> calcularTudo(double capitalInicial, double aporteMensal, List<Ativos> ativos, List<Porcentagens> porcentagens) {
-        List<Feedback> feedbackList = new java.util.ArrayList<>();
+    public List<Double> calcularTudo(double capitalTotal, double aporteMensal, List<Ativos> ativos, List<Porcentagens> porcentagens) {
+        List<Double> feedbackList = new java.util.ArrayList<>();
 
         List<Ativo> ativosBanco = ativoRepository.findAll();
 
         for (int i = 0; i < ativos.size(); i++) {
+            Ativo ativo = ativosBanco.get(i);
+            List<Double> variations = ativo.getVariacoes();
+
+            // Ponderação do peso 
+            double ponderacao = porcentagens.get(i).getPorcentagem() / 100.0;
+
             for (int n = 0; n < ativosBanco.size(); n++) {
-                Ativo ativo = ativosBanco.get(n);
-                List<Double> variations = ativo.getVariacoes();
+
+                // Juros compostos
+
+                double variacao = variations.get(n) / 100.0;
+                capitalTotal *= (1 + variacao * ponderacao);
+                
+
+                // Aportes
+                capitalTotal += aporteMensal * ponderacao;
+
+                // Feedback
+                feedbackList.add(capitalTotal);
+
+                
+    
             }
         }
         return feedbackList;
